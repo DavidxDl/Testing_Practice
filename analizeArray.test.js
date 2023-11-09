7ff6a6a1d58dfc3830f96676aa5d814d6ca78910
David <Davidbekamlopez@gmail.com>
2023-11-09 05:49:50 -0300

    completed the analizedArray and the tests

--- analizeArray.test.js
+++ analizeArray.test.js
@@ -10,3 +10,19 @@ test('analilzeArray 1', () => {
         length: 5
     })
 })
+test('analilzeArray 2', () => {
+    expect(analizeArray([11, 21, 33, 42, 54, 63])).toMatchObject({
+        average: 224 / 6,
+        min: 11,
+        max: 63,
+        length: 6
+    })
+})
+test('analilzeArray 3', () => {
+    expect(analizeArray([79, 33, 87, 65, 212, 33, 66, 123])).toMatchObject({
+        average: 698 / 8,
+        min: 33,
+        max: 212,
+        length: 8
+    })
+})
--- functions/analizeArray.js
+++ functions/analizeArray.js
@@ -1,3 +1,11 @@
-export default function analizeArray(arr) {
-
+export default function analizeArray(arr = []) {
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+    const average = arr.reduce((prev, curr) => prev += curr) / arr.length
+    return {
+        average,
+        min,
+        max,
+        length: arr.length
+    }
 }
